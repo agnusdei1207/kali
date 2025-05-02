c6d27f45abe54b1ac8f82bf6f5c5e7fe7c25bcf4
psw <agnusdei1207@gmail.com>
2025-05-02 11:42:03 +0900

    update compose

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
+
+docker-exec:
+	docker exec -it offensive-security /bin/bash
+	
 docker-push:
-    docker/offensive-security/push.sh
-docker-prune:
-    docker prune -af --volumes
+	docker/offensive-security/push.sh
---
+++ docker/offensive-security/exec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 변수 설정
+IMAGE_NAME="offensive-security"
+TAG="latest"
+DOCKER_USERNAME="agnusdei1207"
+DOCKER_IMAGE="$DOCKER_USERNAME/$IMAGE_NAME:$TAG"
+DOCKERFILE="docker/offensive-security/Dockerfile"
+
+docker exec -it $DOCKER_IMAGE /bin/bash
+
