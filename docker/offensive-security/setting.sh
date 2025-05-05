548c71ed8dfa5bdb10e8129a400afa818e9f1b9f
psw <agnusdei1207@gmail.com>
2025-05-05 22:24:12 +0900

    update

---
+++ docker/offensive-security/setting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update && \
+    apt-get install -y \
+    openvpn \
+    iputils-ping \
+    netcat-openbsd \
+    nmap \
+    openssh-client \
+    procps \
+    psmisc \
+	vim	\
+	net-tools
