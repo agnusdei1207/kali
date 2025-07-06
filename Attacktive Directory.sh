8ca6aa404450c94b2ca119f9925cab553671ee10
agnusdei <agnusdei@agnusdeis-MacBook-Air.local>
2025-07-06 19:46:46 +0900

    update

--- Attacktive Directory.sh
+++ Attacktive Directory.sh
@@ -1,10 +1,10 @@
-10.10.233.27
+10.10.206.91
 
 # nmap
 
-nmap -sV -sC -Pn -T4 --open -oN namp.scan.txt 10.10.233.27
+nmap -sV -sC -Pn -T4 --open -oN namp.scan.txt 10.10.206.91
 
-Nmap scan report for 10.10.233.27
+Nmap scan report for 10.10.206.91
 Host is up (0.29s latency).
 Not shown: 986 closed tcp ports (reset)
 PORT STATE SERVICE VERSION
@@ -51,7 +51,7 @@ Host script results:
 
 Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
 
-Nmap은 네트워크 스캐닝 도구로, 호스트와 네트워크 서비스에 대한 정보를 수집하는 데 사용됩니다. 제공된 Nmap 스캔 결과는 IP 주소 `10.10.233.27`에 대한 상세한 스캔 결과를 보여줍니다. 각 포트와 서비스에 대한 정보를 분석하면 다음과 같습니다:
+Nmap은 네트워크 스캐닝 도구로, 호스트와 네트워크 서비스에 대한 정보를 수집하는 데 사용됩니다. 제공된 Nmap 스캔 결과는 IP 주소 `10.10.206.91`에 대한 상세한 스캔 결과를 보여줍니다. 각 포트와 서비스에 대한 정보를 분석하면 다음과 같습니다:
 
 1. **53/tcp**: DNS 서비스 (Simple DNS Plus)가 열려 있습니다. DNS는 도메인 이름을 IP 주소로 변환하는 데 사용됩니다.
 
@@ -97,7 +97,7 @@ Nmap은 네트워크 스캐닝 도구로, 호스트와 네트워크 서비스에
 
 # /etc/hosts
 
-10.10.233.27 spookysec.local
+10.10.206.91 spookysec.local
 
 # enum4linux
 
@@ -117,53 +117,53 @@ sudo neo4j console
 # enum4linux
 
 (kali㉿vbox)-[~]
-└─$ sudo enum4linux -a 10.10.233.27
+└─$ sudo enum4linux -a 10.10.206.91
 Starting enum4linux v0.9.1 ( http://labs.portcullis.co.uk/application/enum4linux/ ) on Sun Jul 6 03:45:13 2025
 
 =========================================( Target Information )=========================================
 
-Target ........... 10.10.233.27
+Target ........... 10.10.206.91
 RID Range ........ 500-550,1000-1050
 Username ......... ''
 Password ......... ''
 Known Usernames .. administrator, guest, krbtgt, domain admins, root, bin, none
 
-============================( Enumerating Workgroup/Domain on 10.10.233.27 )============================
+============================( Enumerating Workgroup/Domain on 10.10.206.91 )============================
 
 [E] Can't find workgroup/domain
 
-================================( Nbtstat Information for 10.10.233.27 )================================
+================================( Nbtstat Information for 10.10.206.91 )================================
 
-Looking up status of 10.10.233.27
-No reply from 10.10.233.27
+Looking up status of 10.10.206.91
+No reply from 10.10.206.91
 
-===================================( Session Check on 10.10.233.27 )===================================
+===================================( Session Check on 10.10.206.91 )===================================
 
-[+] Server 10.10.233.27 allows sessions using username '', password ''
+[+] Server 10.10.206.91 allows sessions using username '', password ''
 
-================================( Getting domain SID for 10.10.233.27 )================================
+================================( Getting domain SID for 10.10.206.91 )================================
 
 Domain Name: THM-AD  
 Domain Sid: S-1-5-21-3591857110-2884097990-301047963
 
 [+] Host is part of a domain (not a workgroup)
 
-===================================( OS information on 10.10.233.27 )===================================
+===================================( OS information on 10.10.206.91 )===================================
 
 [E] Can't get OS info with smbclient
 
-[+] Got OS info for 10.10.233.27 from srvinfo:  
+[+] Got OS info for 10.10.206.91 from srvinfo:  
 do_cmd: Could not initialise srvsvc. Error was NT_STATUS_ACCESS_DENIED
 
-=======================================( Users on 10.10.233.27 )=======================================
+=======================================( Users on 10.10.206.91 )=======================================
 
 [E] Couldn't find users using querydispinfo: NT_STATUS_ACCESS_DENIED
 
 [E] Couldn't find users using enumdomusers: NT_STATUS_ACCESS_DENIED
 
-=================================( Share Enumeration on 10.10.233.27 )=================================
+=================================( Share Enumeration on 10.10.206.91 )=================================
 
-do_connect: Connection to 10.10.233.27 failed (Error NT_STATUS_RESOURCE_NAME_NOT_FOUND)
+do_connect: Connection to 10.10.206.91 failed (Error NT_STATUS_RESOURCE_NAME_NOT_FOUND)
 
         Sharename       Type      Comment
         ---------       ----      -------
@@ -171,17 +171,17 @@ do_connect: Connection to 10.10.233.27 failed (Error NT_STATUS_RESOURCE_NAME_NOT
 Reconnecting with SMB1 for workgroup listing.
 Unable to connect with SMB1 -- no workgroup available
 
-[+] Attempting to map shares on 10.10.233.27
+[+] Attempting to map shares on 10.10.206.91
 
-============================( Password Policy Information for 10.10.233.27 )============================
+============================( Password Policy Information for 10.10.206.91 )============================
 
 [E] Unexpected error from polenum:
 
-[+] Attaching to 10.10.233.27 using a NULL share
+[+] Attaching to 10.10.206.91 using a NULL share
 
 [+] Trying protocol 139/SMB...
 
-        [!] Protocol failed: Cannot request session (Called Name:10.10.233.27)
+        [!] Protocol failed: Cannot request session (Called Name:10.10.206.91)
 
 [+] Trying protocol 445/SMB...
 
@@ -189,7 +189,7 @@ Unable to connect with SMB1 -- no workgroup available
 
 [E] Failed to get password policy with rpcclient
 
-=======================================( Groups on 10.10.233.27 )=======================================
+=======================================( Groups on 10.10.206.91 )=======================================
 
 [+] Getting builtin groups:
 
@@ -203,7 +203,7 @@ Unable to connect with SMB1 -- no workgroup available
 
 [+] Getting domain group memberships:
 
-==================( Users on 10.10.233.27 via RID cycling (RIDS: 500-550,1000-1050) )==================
+==================( Users on 10.10.206.91 via RID cycling (RIDS: 500-550,1000-1050) )==================
 
 [I] Found new SID:  
 S-1-5-21-3591857110-2884097990-301047963
@@ -240,7 +240,7 @@ S-1-5-21-3591857110-2884097990-301047963-526 THM-AD\Key Admins (Domain Group)
 S-1-5-21-3591857110-2884097990-301047963-527 THM-AD\Enterprise Key Admins (Domain Group)
 S-1-5-21-3591857110-2884097990-301047963-1000 THM-AD\ATTACKTIVEDIREC$ (Local User)
 
-===============================( Getting printer info for 10.10.233.27 )===============================
+===============================( Getting printer info for 10.10.206.91 )===============================
 
 do_cmd: Could not initialise spoolss. Error was NT_STATUS_ACCESS_DENIED
 
